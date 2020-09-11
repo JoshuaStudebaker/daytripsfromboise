d1f6cf0ce5bfc43db38b46b542bfe1ddde4a1813
JoshuaStudebaker <j.s.studebaker1@gmail.com>
2020-09-11 17:21:27 -0600

    client comment work

---
+++ client/app/Models/Comment.js
@@ -0,0 +1,26 @@
+export default class Comment {
+  constructor(body, cVote, user, id, parentId, voters = []) {
+    this.body = body;
+    this.user = user;
+    this.cVote = cVote;
+    this.id = id;
+    // this.commentTime = commentTime;
+    this.parentId = parentId;
+    this.voters = voters;
+  }
+
+  get Template() {
+    return `
+    <div class="card">
+  <div class="card-header">
+   ${this.user}
+  </div>
+  <div class="card-body">    
+    <p class="card-text mb-0">${this.body}</p>
+    <p class="card-text mb-0"> <i class="fas fa-arrow-alt-circle-up fa-lg"></i> votes <i class="fas fa-arrow-alt-circle-down fa-lg"></i></p>
+    <p><i class="fa fa-trash text-right m-2" aria-hidden="true" onclick="app.commentsController.deleteComment('${this.id}')"></i></p>
+  </div>
+</div>
+    `;
+  }
+}
