cd4a3ec0bff286a03a2b7d021273a3eb74691b66
Lawrence Haney <Lawrence.r.haney@gmail.com>
2020-09-11 17:55:10 -0600

    sytling changes

--- client/app/Models/Post.js
+++ client/app/Models/Post.js
@@ -13,7 +13,7 @@ export default class Post{
   }
   get cardtemp(){
     return`
-    <div class="card text-white col-3 appcard m-1 card-img" style="background-image: url(${this.imgUrl})" onclick="app.postsController.focusPost('${this._id}')">
+    <div class="card shadow text-white col-3 appcard m-1 card-img" style="background-image: url(${this.imgUrl})" onclick="app.postsController.focusPost('${this._id}')">
         <div class="card-img-overlay">
           <h4 class="card-title">${this.title}</h4>
           <p class="card-text">${this.user}</p>
@@ -33,7 +33,7 @@ export default class Post{
         <p class= "text-light"><i class="fas fa-arrow-alt-circle-up fa-lg"></i>${this.pVote}<i class="fas fa-arrow-alt-circle-down fa-lg"></i></p></div>
         </div>
     </div>
-    <div class="col-5 card bg-secondary">
+    <div class="col-5 card bg-secondary shadow">
       <i class="fa fa-trash text-right m-2" aria-hidden="true" onclick="app.postsController.removePost('${this._id}')"></i>
       <h1>${this.title}</h1>
       <p>${this.description}</p>
