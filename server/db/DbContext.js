6049c2101520cb18c7dd9306480fc568a52a3c56
CoreyWhitmore <cjwhit97@hotmail.com>
2020-09-11 10:49:26 -0600

    progress on server

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
-import ValueSchema from "../models/Value";
+import PostSchema from "../models/Post";
 import ProfileSchema from "../models/Profile";
 
 class DbContext {
-  Values = mongoose.model("Value", ValueSchema);
+  Posts = mongoose.model("Post", PostSchema);
   Profile = mongoose.model("Profile", ProfileSchema);
 }
 
---
+++ server/services/PostsService.js
@@ -0,0 +1,24 @@
+import { dbContext } from "../db/DbContext";
+import { BadRequest } from "../utils/Errors";
+
+class PostsService {
+  delete(id) {
+    throw new Error("Method not implemented.");
+  }
+  create(body) {
+    throw new Error("Method not implemented.");
+  }
+  async find(query = {}) {
+    let post = await dbContext.Posts.find(query);
+    return post;
+  }
+  async findById(id) {
+    let value = await dbContext.Posts.findById(id);
+    if (!value) {
+      throw new BadRequest("Invalid Id");
+    }
+    return value;
+  }
+}
+
+export const postsService = new PostsService();
