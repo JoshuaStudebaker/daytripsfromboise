3f40c547c2345d23ee1d165c45dfb5b30781d681
Lawrence Haney <Lawrence.r.haney@gmail.com>
2020-09-12 00:39:22 -0600

    changed text stuffs

--- client/app/Models/Post.js
+++ client/app/Models/Post.js
@@ -49,23 +49,25 @@ export default class Post {
 
   get Active() {
     return `
-    <section class="row p-2 justify-content-center ">
-    <div class="col-5 text-center post-body shadow">
+    <section class="row p-2 justify-content-center">
+    <div class="col-5 text-center post-body">
       <div class="img-cont d-flex flex-column justify-content-end align-items-center" style="background-image: url('${this.imgUrl}');">
         <h3 class="text-center text-light text-shadow">Distance: ${this.distance} ~ ${this.timeIcon}</h3>
         <div class="d-flex justify-content-between align-items-center">
         <p class= "text-light text-shadow"><i class="fas fa-arrow-alt-circle-up fa-lg" onclick="app.postsController.vote(1)"></i>${this.pVote}<i class="fas fa-arrow-alt-circle-down fa-lg" onclick="app.postsController.vote(-1)"></i></p></div>
         </div>
     </div>
-    <div class="col-5 card bg-secondary text-light text-shadow shadow">
-      <i class="fa fa-trash text-right m-2" aria-hidden="true" onclick="app.postsController.removePost('${this._id}')"></i>
-      <h1>${this.title}</h1>
+    <div class="col-5 card bg-secondary text-light text-shadow shadow my-2 p-3">
+      <i class="fa fa-trash text-right " aria-hidden="true" onclick="app.postsController.removePost('${this._id}')"></i>
+      <h1><strong>${this.title}</strong></h1>
+      <hr>
       <p>${this.description}</p>
       <h3 class="align-self-end mt-auto p-2 bd-highlight">${this.placeName}</h3>
+      <hr>
       <h6 class="align-self-end mt-auto p-2 bd-highlight">${this.user}</h6>
     </div>
-    <form class="col-10 card px-2 py-1 mx-2 form-group bg-primary" onsubmit="app.commentsController.addComment('${this._id}')">
-        <textarea name="body" cols="10" rows="5"></textarea>
+    <form class="col-10 card px-2 py-4 mx-2 form-group bg-primary  my-1" onsubmit="app.commentsController.addComment('${this._id}')">
+        <textarea class="bg-light text-dark p-2"name="body" cols="10" rows="5"> Have you been there?</textarea>
         <button type="submit" class="btn btn-outline-success text-light">Add Comment</button>
       </form>
   </section>
