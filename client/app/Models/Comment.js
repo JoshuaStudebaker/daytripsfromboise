456b6d700fa7c78e5e1bd164dfe18376a58c9813
JoshuaStudebaker <j.s.studebaker1@gmail.com>
2020-09-12 00:17:25 -0600

    some comment formatting

--- client/app/Models/Comment.js
+++ client/app/Models/Comment.js
@@ -12,17 +12,22 @@ export default class Comment {
 
   get Template() {
     return `
-    <div class="card">
-  <div class="card-header">
-   ${this.user}
-  </div>
+    <div class="card"> 
   <div class="card-body">    
-    <p class="card-text mb-0">${this.body}</p>
-    <p class="card-text mb-0"> <i class="fas fa-arrow-alt-circle-up fa-lg" onclick="app.commentsController.likeComment(1, '${this._id}')"></i> votes <i class="fas fa-arrow-alt-circle-down fa-lg"></i></p>
-    <p><i class="fa fa-trash text-right m-2" aria-hidden="true" onclick="app.commentsController.deleteComment('${this._id}')"></i></p>
-    <p>${this.cVote}</p>
+    <p class="card-text mb-3 shadow p-3"><span class="text-muted"> ${this.user} commented: ${this.body}  </p>
+    <p onclick="app.commentsController.likeComment(1, '${this._id}')" class="card-text mb-1 heart-pointer"> 💕 Give Some Love 💕</p>
+    <p><i class="fas fa-heart heart"></i>: ${this.cVote}${this.Exclamation}</p>
+    <p class="text-right"><i class="fa fa-trash text-right m-2" aria-hidden="true" onclick="app.commentsController.deleteComment('${this._id}')"></i></p>
   </div>
 </div>
     `;
+  }
+
+  get Exclamation() {
+    if (this.cVote >= 5) {
+      return `!`;
+    } else {
+      return ``;
+    }
   }
 }
